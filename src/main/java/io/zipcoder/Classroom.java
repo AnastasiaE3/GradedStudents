f97fe74e4eecb8878dcaaa4acd067276a74a9e3e
AnastasiaE3 <125825396+AnastasiaE3@users.noreply.github.com>
2023-04-22 16:32:54 -0400

    added changes

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,77 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Classroom {
+    Student[] students;
+    public Classroom (int maxNumberOfStudents) {
+        students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom (int maxNumberOfStudents, Student... student) {
+        students = new Student[maxNumberOfStudents];
+        int counter = 0;
+        for (Student s: student) {
+            students[counter] = s;
+            counter++;
+        }
+    }
+
+    public Classroom () {
+        students = new Student[30];
+    }
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore() {
+        double sum = 0.0;
+        for (Student s : students) {
+            sum += s.getAverageExamScore();
+        }
+        return sum/students.length;
+    }
+
+    public void addStudent(Student student) {
+        for (int i = 0; i < students.length; i ++){
+            if (students[i] == null){
+                students[i] = student;
+                return;
+            }
+        }
+        System.out.println("error, class is full");
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+        int counter = 0;
+        for (int i = 0; i < students.length; i++ ) {
+            counter ++;
+            if(students[i].getLastName().equals(lastName) && students[i].getFirstName().equals(firstName)){
+                students[i] = null;
+                break;
+            }
+        }
+        for (int i = counter - 1; i < students.length -1; i++) {
+            students[i] = students[i + 1];
+        }
+        students[students.length -1] = null;
+    }
+
+    public Student[] getStudentsByScore(double score) {
+        ArrayList<Student> s = new ArrayList<>();
+        for (Student ss : students) {
+            if (ss != null) {
+                if (ss.getAverageExamScore() == score) {
+                    s.add(ss);
+                }
+            }
+        }
+        Collections.sort(s);
+        return s.toArray(new Student[0]);
+    }
+
+//    public Student[] getGradeBook() {
+//
+//    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+    @Test
+    public void testGetStudents() {
+        Student s1 = new Student("Serah", "Tutelkin", new Double[]{150.0});
+        Student s2 = new Student("Ana", "Jopa", new Double[]{100.0});
+        Student s3 = new Student("Andrey", "Sosulkin", new Double[]{67.0});
+        Classroom croom = new Classroom(3,s1, s2,s3);
+
+        Student[] actual = croom.getStudents();
+
+        Assert.assertEquals(actual[0], s1);
+        Assert.assertEquals(actual[1], s2);
+        Assert.assertEquals(actual[2], s3);
+
+
+    }
 }
